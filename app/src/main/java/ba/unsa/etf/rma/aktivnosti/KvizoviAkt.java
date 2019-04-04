a0868f846a40b67b1e13c702cd33de518a747b4c
lmehmedagi1 <lmehmedagi1@etf.unsa.ba>
2019-04-04 20:57:39 +0200

    dodan adapter za kvizove

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import ba.unsa.etf.rma.R;
 
-public class MainActivity extends AppCompatActivity {
+public class KvizoviAkt extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_kvizovi_akt);
     }
 }
---
+++ app/src/main/java/ba/unsa/etf/rma/klase/KvizAdapter.java
@@ -0,0 +1,37 @@
+package ba.unsa.etf.rma.klase;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ba.unsa.etf.rma.R;
+
+public class KvizAdapter extends ArrayAdapter<Kviz> {
+    
+    public KvizAdapter(Context context, List<Kviz> kvizovi) {
+        super(context, R.layout.element_liste, kvizovi);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View customView = inflater.inflate(R.layout.element_liste, parent, false);
+
+        Kviz kviz = getItem(position);
+        TextView tekst = (TextView) customView.findViewById(R.id.tekst);
+        ImageView ikona = (ImageView) customView.findViewById(R.id.ikona);
+
+        tekst.setText(kviz.getNaziv());
+        ikona.setImageResource(R.drawable.item);
+
+        return customView;
+    }
+}
